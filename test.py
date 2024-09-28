b71a707048e6234d71b8a7510d8086ae80da55a8
Aditya Tripathi <aditya.tripathi.beni@gmail.com>
2024-09-28 11:37:24 +0530

    Initial commit

---
+++ test.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from ics import Calendar, Event
+import openai
+
+print(dir(openai))
+c = Calendar()
+e = Event()
+
+e.summary = "Take your morning medication"
+e.description = "Hello world"
+e.begin = datetime.fromisoformat("2024-09-28T12:00:00+05:30")
+e.end = datetime.fromisoformat("2024-09-28T13:00:00+05:30")
+c.events.add(e)
+
+with open("test.ics","w") as f:
+    f.write(c.serialize())
